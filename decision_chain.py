23671eb7031c0dfbdea7deed2f148866b478c89b
Pratik-prog-gif <ghoshpratik270@gmail.com>
2025-08-08 23:41:25 +0530

    Update decision_chain.py

--- decision_chain.py
+++ decision_chain.py
@@ -1,8 +1,8 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 import os
-import streamlit as st
-api_key = st.secrets["API_KEY"]
+api_key = os.getenv("API_KEY")
+
 
 
 def get_decision_chain():
@@ -31,5 +31,6 @@ def get_decision_chain():
     ])
 
     return prompt | llm
+
 
 
